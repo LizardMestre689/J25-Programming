6e62d748ddbe6f89342a304d41781568234eb040
LizardMestre689 <144990985+LizardMestre689@users.noreply.github.com>
2023-11-09 08:34:27 +0100

    Add files via upload

---
+++ arduino/Serial_Reader_1_Cooking_system_3_Own_Recipe_copy.ino
@@ -0,0 +1,49 @@
+//Serial Reader
+int pan = 1;
+int ajo = 0;
+int tomato = 1;
+bool oil = true;
+double sal = 1;
+ 
+void setup() {
+ Serial.begin(9600);
+}
+ 
+void loop() {
+ String input = Serial.readString();
+ if (input != "") {
+  Serial.println();
+ Serial.println("Tu as escrito: " + input);
+ input.trim();
+ decide(input);
+ }
+}
+ 
+ void decide(String input) {
+ if (input == "cook") {
+ cook();
+ }
+ if(input == "pan") {
+  pan++;
+ }
+  if(input == "ajo") {
+  ajo++;
+ }
+  if(input == "sal") {
+  sal++;
+ }
+  if(input == "tomate") {
+  tomate++;
+ }
+ }
+
+void cook() {
+ Serial.println("We are cooking");
+ if (pan >= 1 && ajo >= 1 && sal >= 0.25 && tomate >= 1 && oil ) {
+ Serial.println("you have Pan con tomate");
+ pan = pan - 1;
+ ajo--;
+ sal = sal - 0.25;
+ tomate--;
+ }
+}
