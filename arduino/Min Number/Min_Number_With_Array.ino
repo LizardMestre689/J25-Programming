8a049f21dc5305a33b12b4c677288bc0d81b5393
LizardMestre689 <144990985+LizardMestre689@users.noreply.github.com>
2023-12-04 08:40:12 +0100

    Update Min_Number_With_Array.ino

--- arduino/Min Number/Min_Number_With_Array.ino
+++ arduino/Min Number/Min_Number_With_Array.ino
@@ -1,17 +1,17 @@
-
-  void setup(){
-
+void setup(){
+Serial.begin(9600);
     int grades [] = {9,5,8,3,2,9,3,1,7};
   int min = grades[0];
-  for (int i = 0; i < 8; i++) {
+  for (int i = 0; i < 9; i++) {
     if (grades[i] < min) {
      min = grades[i];
-     //Writh the min number with a delay
-  Serial.println(i);
-  delay(500);
     }
   }
-}
+ //Writh the min number with a delay
+    Serial.println(min);
+    delay(500);
+    }
+
 
 void loop(){
 }
