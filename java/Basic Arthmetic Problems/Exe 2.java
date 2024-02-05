259effbb9605c27a3602b9814ede0556e7b2ecd1
LizardMestre689 <144990985+LizardMestre689@users.noreply.github.com>
2024-02-05 09:59:14 +0100

    Create Exe 2.java

---
+++ java/Basic Arthmetic Problems/Exe 2.java
@@ -0,0 +1,13 @@
+/*
+ * Test Arithmetic Operations
+ */
+public class Main {     // Save as "ArithmeticTest.java"
+   public static void main(String[] args) {
+      int number1 = 98; // Declare an int variable number1 and initialize it to 98
+      int number2 = 5;  // Declare an int variable number2 and initialize it to 5
+      int sum;  // Declare 5 int variables to hold results
+sum = (31 * number1) + (17 * number2);
+   
+      System.out.println(" La suma de 31 veces " + number1 + " 17 veces " + number2 + " es " + sum); 
+   }
+}
