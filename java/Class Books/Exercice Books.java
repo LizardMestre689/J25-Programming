6d048162aed5441c27f3d5a5dee59f4d7344236c
LizardMestre689 <144990985+LizardMestre689@users.noreply.github.com>
2024-03-07 09:04:46 +0100

    Update Exercice Books.java

--- java/Class Books/Exercice Books.java
+++ java/Class Books/Exercice Books.java
@@ -1,8 +1,31 @@
-
 public class Book {
-  private String titleOfBook; 
-  private String authorOfBook;
-   private int numberOfPages;
-   private String ISBN;
-   private boolean itsInKorean;
+   private String title; 
+   private String author;
+    private int pages;
+    private String ISBN;
+    private boolean inKorean;
+    
+    public Book() {
+        this.title = "untitled";
+        this.author = "anonimous";
+        this.pages = 0;
+        this.ISBN = "none";
+        this.inKorean = false;
+    }
+    
+    public Book (String title) {
+        this.title = title;
+        this.author = "anonimous";
+        this.pages = 0;
+        this.ISBN = "none";
+        this.inKorean = false;     
+    }
+    
+    public Book (String title, int numberOfPages) {
+        this.title = title;
+        this.author = "anonimous";
+        this.pages = numberOfPages;
+        this.ISBN = "none";
+        this.inKorean = false;     
+    }
 }
