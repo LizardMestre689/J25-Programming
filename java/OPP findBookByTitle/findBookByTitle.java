a0efc21196949659a1d3537e8e51a447571b297b
LizardMestre689 <144990985+LizardMestre689@users.noreply.github.com>
2024-03-16 22:28:43 +0100

    Update findBookByTitle.java

--- java/OPP findBookByTitle/findBookByTitle.java
+++ java/OPP findBookByTitle/findBookByTitle.java
@@ -1,1 +1,9 @@
-
+  
+  public Book findBookByTitle(String title) {
+    for (int i = 0; i < library.length; i++) {
+        if (library[i].getTitle().equals(title)) { // We change getISBN for getTitle, and the same with equals()
+            return library[i];
+        }
+    }
+    return null;
+}
