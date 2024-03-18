1adfc07b3d61a700bfa98f26575d38272f5df29c
LizardMestre689 <144990985+LizardMestre689@users.noreply.github.com>
2024-03-18 09:46:33 +0100

    Create findAuthorInLibrary.java

---
+++ java/Exercices OPP/findAuthorInLibrary.java
@@ -0,0 +1,12 @@
+//In this case we're going to implement the method findAuthorInLibrary that will find a book in array of the library of the deparment with the author as a parameter. 
+//If it finds something that has the author is going to return true, if not it's going to return false.
+
+
+Public boolean findAuthorInLibrary (String author) {
+  for(int i = 0; i < library.length ; i++){
+    if (findAuthorInLibrary){
+      return true
+        }
+    }
+      return false
+}
